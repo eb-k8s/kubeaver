709b611d7e48a2660df72ee97849100fdc47cd0a
wangfangling <wangfangling@e-byte.com>
2025-04-08 14:57:50 +0800

    fix bugs

--- frontend/src/api/resources.ts
+++ frontend/src/api/resources.ts
@@ -1,19 +1,8 @@
 import axios from 'axios';
 
-// export function getResourcesList() {
-//   return axios.get('/api/resources');
-// }
-
 export function getResources() {
   return axios.get('/api/resources/offline');
 }
-
-// export function getResourcesDetail(package_name: string) {
-//   // return axios.get(`/api/resources/detail/${package_name}`);
-//   return axios.get<any>(`/api/resources/detail`, {
-//     params: { package_name },
-//   });
-// }
 
 export function downloadConfig(id: any) {
   return axios.get<any>(`/api/config`, {
