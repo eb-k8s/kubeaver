d2f2c25818e3988cac072cf295db4c6bbeba907a
Waylon-cn <2541197813@qq.com>
2025-05-19 17:00:35 +0800

    remove restart flannel

--- backend/ansible/kubespray_process.py
+++ backend/ansible/kubespray_process.py
@@ -405,10 +405,10 @@ def main():
       delegate_to: "{{ item }}"
       ignore_errors: yes
 
-    - name: Restart first flannel containers
-      shell: "{{bin_dir}}/crictl ps | grep kube-flannel | awk '{print $1}' | xargs {{bin_dir}}/crictl stop"
-      delegate_to: "{{ groups['kube_control_plane'] | first }}"
-      ignore_errors: yes
+#   - name: Restart first flannel containers
+#      shell: "{{bin_dir}}/crictl ps | grep kube-flannel | awk '{print $1}' | xargs {{bin_dir}}/crictl stop"
+#      delegate_to: "{{ groups['kube_control_plane'] | first }}"
+#      ignore_errors: yes
     """
     modified_lines = insert_line_after_pattern(lines, """- { role: kubernetes/preinstall, when: "dns_mode != 'none' and resolvconf_mode == 'host_resolvconf'", tags: resolvconf, dns_late: true }""", new_string)
     # 将更改写入文件
