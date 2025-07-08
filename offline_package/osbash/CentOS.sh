645decd380601134c44f4520a15d4e91571879c5
xiaolongwen <77148298+Waylon-cn@users.noreply.github.com>
2025-07-08 11:02:21 +0800

    Feature (#264)
    * add logo
    
    * fix a bug
    
    * add v1.31
    
    * add 2.28
    
    * add workflow
    
    * add workflow
    
    * add workflow
    
    * add workflow
    
    * add workflow
    
    * add workflow
    
    * add workflow
    
    * fix
    
    * fix

--- offline_package/osbash/CentOS.sh
+++ offline_package/osbash/CentOS.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# 首先对CentOS7的yum进行换源，使用阿里云的yum源
+sed -i 's|^mirrorlist=|#mirrorlist=|g' /etc/yum.repos.d/CentOS-*
+sed -i 's|^#baseurl=http://mirror.centos.org|baseurl=https://mirrors.aliyun.com|g' /etc/yum.repos.d/CentOS-*
+
 yum clean all
 yum makecache
 # 安装createrepo
@@ -18,8 +22,21 @@ done
 
 createrepo $CACHEDIR
 
-# 用tar打包为extend_CentOS_7_Core.tgz
-tar -czf extend_CentOS_7_Core.tgz $CACHEDIR
-
+# 设置变量，描述新建目录名
+EXTEND_DIR=CentOS_7_Core
+# 新建目录
+mkdir -p extend_$EXTEND_DIR/repo_files/$EXTEND_DIR
+# 先重命名为repo
+mv $CACHEDIR repo
+# 用tar打包为repo.tgz
+tar -czf repo.tgz repo
+# 将repo.tgz放入repo_files目录下
+mv repo.tgz extend_$EXTEND_DIR/repo_files/$EXTEND_DIR
+# 将repo/pkgs目录下的tar开头的文件拷贝到extend_$EXTEND_DIR/repo_files/$EXTEND_DIR目录下
+cp repo/tar*.rpm extend_$EXTEND_DIR/repo_files/$EXTEND_DIR
+# 并且将该文件重命名为tar.rpm
+mv extend_$EXTEND_DIR/repo_files/$EXTEND_DIR/tar*.rpm extend_$EXTEND_DIR/repo_files/$EXTEND_DIR/tar.rpm
+# 将extend_$EXTEND_DIR压缩为extend_$EXTEND_DIR.tgz
+tar -czf ../extend_$EXTEND_DIR.tgz extend_$EXTEND_DIR
 
 echo "create-repo done."
